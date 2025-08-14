d6dd2f4021591939a5296f7d185a5c6151c79ad5
eres2k <eeesener@amazon.com>
2025-08-14 02:38:50 +0200

    goo

--- src/components/Common/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <div className="w-12 h-12 border-4 border-gray-200 border-t-orange-500 rounded-full animate-spin"></div>
-    </div>
-  );
-}
--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_AUSTIN_URL = 'https://safety.amazon.com/austin-case-study';
-
-export const SEVERITY_COLORS = {
-  'A': '#B71C1C',
-  'B': '#FF5722',
-  'C': '#FFC107',
-  'D': '#4CAF50',
-  'Unknown': '#9E9E9E'
-};
